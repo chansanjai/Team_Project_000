baed13ef3a110d4ea49f1de9d5553abf1bf1ce30
chandrusanjai <chandrusanjai30@gmail.com>
2024-11-26 11:47:55 -0800

    add file

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
+const mysql =require('mysql')
+const db=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'sudentdb'
+})
 const port = 8080
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
